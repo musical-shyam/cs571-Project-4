5a8751df7732e28753eec29d23442a3f9f5004b5
skannan <skannan@cs.binghamton.edu>
2025-11-12 23:57:52 -0500

    completed part 1

--- src/AutomatonImpl.java
+++ src/AutomatonImpl.java
@@ -35,37 +35,70 @@ public class AutomatonImpl implements Automaton {
     @Override
     public void addState(int s, boolean is_start, boolean is_accept) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        if (is_start) {
+            start_states.add(s);
+        }
+        if (is_accept) {
+            accept_states.add(s);
+        }
+        //throw new UnsupportedOperationException("Unimplemented method 'addState'");
     }
 
     @Override
     public void addTransition(int s_initial, char label, int s_final) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+        StateLabelPair key = new StateLabelPair(s_initial, label);
+        if (!transitions.containsKey(key)) {
+            transitions.put(key, new HashSet<Integer>());
+        }
+        transitions.get(key).add(s_final);
+        //throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
     }
 
     @Override
     public void reset() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+        current_states = new HashSet<>(start_states);
+        //throw new UnsupportedOperationException("Unimplemented method 'reset'");
     }
 
     @Override
     public void apply(char input) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        HashSet<Integer> new_states = new HashSet<Integer>();
+        for (int state : current_states) {
+            StateLabelPair key = new StateLabelPair(state, input);
+            if (transitions.containsKey(key)) {
+                new_states.addAll(transitions.get(key));
+            }
+        }
+        current_states = new_states;
+        //throw new UnsupportedOperationException("Unimplemented method 'apply'");
     }
 
     @Override
     public boolean accepts() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        for (int state : current_states) {
+            if (accept_states.contains(state)) {
+                return true;
+            }
+        }
+        return false;
+        //throw new UnsupportedOperationException("Unimplemented method 'accepts'");
     }
 
     @Override
     public boolean hasTransitions(char label) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasTransitions'");
+        for (int state : current_states) {
+            StateLabelPair key = new StateLabelPair(state, label);
+            if (transitions.containsKey(key) && !transitions.get(key).isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+        //throw new UnsupportedOperationException("Unimplemented method 'hasTransitions'");
     }
 
 }
--- src/CompilerFrontendImpl.java
+++ src/CompilerFrontendImpl.java
@@ -18,10 +18,47 @@ public class CompilerFrontendImpl extends CompilerFrontend {
      * DIV: /
      * WHITE_SPACE (' '|\n|\r|\t)*
      */
+    private Automaton makeSingleCharToken(char c) {
+        AutomatonImpl a = new AutomatonImpl();
+        a.addState(0, true, false);
+        a.addState(1, false, true);
+        a.addTransition(0, c, 1);
+        return a;
+    }
     @Override
     protected void init_lexer() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init_lexer'");
+        lex = new LexerImpl();
+        
+        Automaton numAutomaton = new AutomatonImpl();
+        // Define states
+        numAutomaton.addState(0, true, false); // start state
+        numAutomaton.addState(1, false, false);
+        numAutomaton.addState(2, false, true); // accept state
+
+        // Define transitions for NUM: [0-9]*\.[0-9]+
+        for (char c = '0'; c <= '9'; c++) {
+            numAutomaton.addTransition(0, c, 0); // loop on digits before
+            numAutomaton.addTransition(1, c, 2); // digits after decimal
+            numAutomaton.addTransition(2, c, 2); // loop on digits after decimal
+        }
+        numAutomaton.addTransition(0, '.', 1); // transition on decimal point
+        lex.add_automaton(TokenType.NUM, numAutomaton);
+        lex.add_automaton(TokenType.PLUS, makeSingleCharToken('+'));
+        lex.add_automaton(TokenType.MINUS, makeSingleCharToken('-'));
+        lex.add_automaton(TokenType.TIMES, makeSingleCharToken('*'));
+        lex.add_automaton(TokenType.DIV, makeSingleCharToken('/'));
+        lex.add_automaton(TokenType.LPAREN, makeSingleCharToken('('));
+        lex.add_automaton(TokenType.RPAREN, makeSingleCharToken(')'));
+
+        AutomatonImpl whiteSpace = new AutomatonImpl();
+        whiteSpace.addState(0, true, true); // start and accept state
+        for (char c : new char[] {' ', '\n', '\r', '\t'}) {
+            whiteSpace.addTransition(0, c, 0); // loop on whitespace characters
+        }
+        lex.add_automaton(TokenType.WHITE_SPACE, whiteSpace);
+        
+        //throw new UnsupportedOperationException("Unimplemented method 'init_lexer'");
     }
 
 }
